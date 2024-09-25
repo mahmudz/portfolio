ad4e6e10ed63502a25f0c001d4823c8a4481d577
Mahmudul Hasan <mahmudbappy.pri@gmail.com>
2024-09-26 03:22:21 +0600

    feat: skills section added

--- src/app/(public)/about/page.tsx
+++ src/app/(public)/about/page.tsx
@@ -2,6 +2,12 @@ import { getProfile } from "@/sanity/sanity.query";
 import type { ProfileType } from "@/types";
 import { PortableText } from "@portabletext/react";
 
+function SkillCard({ skill }: { skill: string }) {
+  return (
+    <div className="capitalize bg-slate-800 px-3 py-1 rounded-md">{skill}</div>
+  );
+}
+
 export default async function About() {
   const profile: ProfileType = await getProfile();
 
@@ -10,6 +16,18 @@ export default async function About() {
       <article className="prose dark:prose-invert">
         <PortableText value={profile.fullBio} />
       </article>
+
+      <section>
+        <div className="pt-10 pb-4">
+          <h4 className="font-semibold">Things I'm most proficient with</h4>
+        </div>
+
+        <div className="flex flex-wrap gap-2">
+          {profile.skills.map((skill, index) => (
+            <SkillCard skill={skill} key={index} />
+          ))}
+        </div>
+      </section>
 
       <section>
         <h4 className="font-semibold pt-10 pb-4">Connect</h4>
